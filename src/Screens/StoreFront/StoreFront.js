ab85d8d25e2052d74d4cabc2a9175715bdd4f7a0
Anurag Affection <anuragaffection07@gmail.com>
2024-09-13 22:55:09 +0530

    add customer in store front changes

--- src/Screens/StoreFront/StoreFront.js
+++ src/Screens/StoreFront/StoreFront.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import AddCustomer from './AddCustomer'
 import MakeBill from './MakeBill'
+import CustomerInvoices from './CustomerInvoices'
+
 const StoreFront = () => {
   return (
     <div>
       <MakeBill/>
+      <AddCustomer/>
+      <CustomerInvoices/>
     </div>
   )
 }
